db26d037405f47ee9bf4fbaf84ffe2ac66dbab3c
Rutar Andriy <RutarAndriy@gmail.com>
2022-05-14 23:24:34 +0300

    Update trick - Nemo Add Custom Actions

---
+++ Nemo Add Custom Actions/extract_icons/uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Переходимо у папку скриптів
+cd ~/.local/share/nemo/actions
+
+# Видаляємо файл скрипта
+rm extract_icons.nemo_action
+
+# Видаляємо файл скрипта
+rm scripts/extract_icons.sh
