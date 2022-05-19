8b89551794f56cb1f38c0b024839c7428c42e908
Rutar Andriy <RutarAndriy@gmail.com>
2022-05-19 23:03:49 +0300

    Update trick - Wine Setup

--- Wine Setup/Get_Bottles_List.sh
+++ Wine Setup/Get_Bottles_List.sh
@@ -5,12 +5,12 @@ function get_bottles_list {
 
 # Переходимо в домашню директорію
 cd ~
+# Записуємо шлях домашньої директорії
+local USER=$(pwd)
 # Зберігаємо розділювач файлів
-IFS_ORIG=$IFS
+local IFS_ORIG=$IFS
 # Змінюємо розділювач файлів
 IFS=$'\n'
-# Записуємо шлях домашньої директорії
-USER=$(pwd)
 
 # Проходимося циклом по основних wine-директоріях
 for DIR in $USER "$USER/.local/share/wineprefixes" "$USER/.photoshopCCV19"; do
@@ -18,7 +18,7 @@ for DIR in $USER "$USER/.local/share/wineprefixes" "$USER/.photoshopCCV19"; do
     # Задаємо директорію
     cd "$DIR"
     # Записуємо шлях директорії
-    CUR_DIR=$(pwd)
+    local CUR_DIR=$(pwd)
 
     # Проходимося по всіх файлах у директорії
     for FILE in $(ls -a); do
